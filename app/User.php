43b1bd45eda641885fd7301e744844275a51cd4c
anasibrahimgm <anasibrahimgm@gmail.com>
2017-07-07 20:17:28 +0200

    002. Change User table name

--- app/User.php
+++ app/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
+    protected $table = 'users_one';
 
     /**
      * The attributes that are mass assignable.
