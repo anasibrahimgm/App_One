40cd27ccfe1f0e8f0002ad93b516865357e8cf96
anasibrahimgm <anasibrahimgm@gmail.com>
2017-07-08 01:01:06 +0200

    014. confirmation.resend route configuration

--- vendor/bestmomo/laravel-email-confirmation/routes/web.php
+++ vendor/bestmomo/laravel-email-confirmation/routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
-// Email confirmation 
-Route::get('confirmation/resend', 'Auth\RegisterController@resend');
+// Email confirmation
+Route::get('confirmation/resend', 'Auth\RegisterController@resend')->name('confirmation.resend');
 Route::get('confirmation/{id}/{token}', 'Auth\RegisterController@confirm');
